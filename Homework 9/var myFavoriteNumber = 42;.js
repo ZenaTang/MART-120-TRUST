670b1a8c2c67718b6cd37113f3c68e1cf85d3ab5
ZenaTang <shadowkatfeline@gmail.com>
2025-11-05 16:27:39 -0700

    Home work 9

---
+++ Homework 9/var myFavoriteNumber = 42;.js
@@ -0,0 +1,15 @@
+    var myFavoriteNumber = 42;
+     // this function is called only once
+    function setup()
+    {
+        createCanvas(800,600);
+    }
+    /* this function is called continuously
+        while the sketch is open in the browser
+    */
+    function draw()
+    {
+        background(120);
+        myFavoriteNumber++;
+        console.log(myFavoriteNumber);
+    }
