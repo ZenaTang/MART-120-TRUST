623a3812c402cce7c036d4282512eeb2fa755b3e
ZenaTang <shadowkatfeline@gmail.com>
2025-11-24 19:28:06 -0700

    Homework 13

---
+++ Homework 9/sketch.js
@@ -0,0 +1,17 @@
+  var x = 50;
+    var y = 50;
+    var diameter = 25;
+    var x1 = 150;
+    var y1 = 150;
+    var diameter1 = 125;
+
+    function setup()
+    {
+        createCanvas(800,600);
+    }
+
+    function draw()
+    {
+        circle(x,y,diameter);
+        circle(x1,y1,diameter1)
+    }
